911f6d78673664c62e6c645c5a859b61e5c7ed07
junanddark <junanddark@gmail.com>
2026-05-21 09:26:03 +0700

    Add files via upload

--- data-toko.js
+++ data-toko.js
@@ -609,6 +609,8 @@ const listToko = [
 { kode: "TT51", nama: "CIKADUEN" },
 { kode: "TXV2", nama: "GAJRUG" },
 { kode: "TZ4V", nama: "RAYA KAGUNGAN " },
-{ kode: "TZKS", nama: "PARIGI CIKANDE" }
+{ kode: "TZKS", nama: "PARIGI CIKANDE" },
+{ kode: "TB3S", nama: " RAYA BAROS SERANG" }, 
+{ kode: "T2TG", nama: " RAYA PALKA PABUARAN" }
 
 ];
